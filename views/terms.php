16358afc0cfb7f6bf778bf7f8e660ed83610f28c
agservizi <ag.servizi16@gmail.com>
2026-02-01 11:39:29 +0100

    feat: ristruttura la vista "Termini e condizioni" con una nuova struttura e contenuti

--- views/terms.php
+++ views/terms.php
@@ -4,103 +4,105 @@ declare(strict_types=1);
 $pageTitle = $pageTitle ?? 'Termini e condizioni';
 ?>
 <section class="page">
-    <header class="page__header">
-        <h2>Termini e condizioni</h2>
-        <p>Le presenti condizioni regolano l’uso del software e dei servizi forniti da AG SERVIZI.</p>
-    </header>
+    <div class="card">
+        <header class="page__header">
+            <h2>Termini e condizioni</h2>
+            <p>Le presenti condizioni regolano l’uso del software e dei servizi forniti da AG SERVIZI.</p>
+        </header>
 
-    <section class="page__section">
-        <h3>1. Titolare e contatti</h3>
-        <p>
-            Sviluppatore e titolare: Cavaliere Carmine – AG SERVIZI<br>
-            Sede legale: Via Plinio il Vecchio 72, 80053 Castellammare di Stabia (NA)<br>
-            P. IVA / C.F.: IT08442881218<br>
-            Tel: 081 058 4542 – WhatsApp: 377 379 8570<br>
-            Email: nightcoder88@gmail.com<br>
-            PEC: agserviziviaplinio@sicurezzapostale.it
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>1. Titolare e contatti</h3>
+            <p>
+                Sviluppatore e titolare: Cavaliere Carmine – AG SERVIZI<br>
+                Sede legale: Via Plinio il Vecchio 72, 80053 Castellammare di Stabia (NA)<br>
+                P. IVA / C.F.: IT08442881218<br>
+                Tel: 081 058 4542 – WhatsApp: 377 379 8570<br>
+                Email: nightcoder88@gmail.com<br>
+                PEC: agserviziviaplinio@sicurezzapostale.it
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>2. Oggetto</h3>
-        <p>
-            Il presente documento disciplina l’accesso e l’utilizzo del software gestionale (di seguito “Software”),
-            della relativa documentazione e dei servizi correlati.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>2. Oggetto</h3>
+            <p>
+                Il presente documento disciplina l’accesso e l’utilizzo del software gestionale (di seguito “Software”),
+                della relativa documentazione e dei servizi correlati.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>3. Proprietà intellettuale e diritti d’autore</h3>
-        <p>
-            Il Software è protetto dalla normativa vigente in materia di diritto d’autore
-            (Legge 22 aprile 1941 n. 633 e successive modifiche) e dalle ulteriori disposizioni applicabili.
-            Tutti i diritti di proprietà intellettuale, inclusi codice sorgente, architettura, layout, interfacce,
-            loghi e contenuti, sono riservati ad AG SERVIZI o ai rispettivi titolari terzi.
-        </p>
-        <p>
-            L’utente riconosce che l’uso del Software non comporta alcun trasferimento di diritti di proprietà
-            intellettuale o concessioni diverse da quelle esplicitamente indicate in questi termini.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>3. Proprietà intellettuale e diritti d’autore</h3>
+            <p>
+                Il Software è protetto dalla normativa vigente in materia di diritto d’autore
+                (Legge 22 aprile 1941 n. 633 e successive modifiche) e dalle ulteriori disposizioni applicabili.
+                Tutti i diritti di proprietà intellettuale, inclusi codice sorgente, architettura, layout, interfacce,
+                loghi e contenuti, sono riservati ad AG SERVIZI o ai rispettivi titolari terzi.
+            </p>
+            <p>
+                L’utente riconosce che l’uso del Software non comporta alcun trasferimento di diritti di proprietà
+                intellettuale o concessioni diverse da quelle esplicitamente indicate in questi termini.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>4. Licenza d’uso</h3>
-        <p>
-            È concessa una licenza d’uso non esclusiva, non trasferibile e revocabile, limitata al periodo di validità
-            della licenza acquistata e all’ambito interno dell’organizzazione autorizzata.
-        </p>
-        <p>
-            È vietato, a titolo esemplificativo e non esaustivo:
-        </p>
-        <ul>
-            <li>copiare, riprodurre o distribuire il Software o parti di esso, salvo ove espressamente consentito;</li>
-            <li>modificare, decompilare, disassemblare o effettuare reverse engineering;</li>
-            <li>rimuovere o alterare indicazioni di copyright, marchi o avvisi di titolarità;</li>
-            <li>concedere in sublicenza, vendere o trasferire a terzi l’accesso non autorizzato al Software.</li>
-        </ul>
-    </section>
+        <section class="page__section">
+            <h3>4. Licenza d’uso</h3>
+            <p>
+                È concessa una licenza d’uso non esclusiva, non trasferibile e revocabile, limitata al periodo di validità
+                della licenza acquistata e all’ambito interno dell’organizzazione autorizzata.
+            </p>
+            <p>
+                È vietato, a titolo esemplificativo e non esaustivo:
+            </p>
+            <ul>
+                <li>copiare, riprodurre o distribuire il Software o parti di esso, salvo ove espressamente consentito;</li>
+                <li>modificare, decompilare, disassemblare o effettuare reverse engineering;</li>
+                <li>rimuovere o alterare indicazioni di copyright, marchi o avvisi di titolarità;</li>
+                <li>concedere in sublicenza, vendere o trasferire a terzi l’accesso non autorizzato al Software.</li>
+            </ul>
+        </section>
 
-    <section class="page__section">
-        <h3>5. Uso lecito e responsabilità dell’utente</h3>
-        <p>
-            L’utente si impegna a utilizzare il Software in conformità alle leggi italiane, ai presenti termini e alle
-            istruzioni d’uso. L’utente è responsabile dei dati inseriti, trattati o caricati nel sistema, nonché delle
-            credenziali di accesso assegnate.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>5. Uso lecito e responsabilità dell’utente</h3>
+            <p>
+                L’utente si impegna a utilizzare il Software in conformità alle leggi italiane, ai presenti termini e alle
+                istruzioni d’uso. L’utente è responsabile dei dati inseriti, trattati o caricati nel sistema, nonché delle
+                credenziali di accesso assegnate.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>6. Limitazione di responsabilità</h3>
-        <p>
-            Il Software è fornito “così com’è”. AG SERVIZI si impegna a mantenere il servizio operativo con la dovuta
-            diligenza, fermo restando che non garantisce l’assenza di interruzioni o errori. Nei limiti consentiti dalla
-            legge, AG SERVIZI non è responsabile per danni indiretti, perdita di dati o mancati profitti derivanti
-            dall’uso o dall’impossibilità di usare il Software.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>6. Limitazione di responsabilità</h3>
+            <p>
+                Il Software è fornito “così com’è”. AG SERVIZI si impegna a mantenere il servizio operativo con la dovuta
+                diligenza, fermo restando che non garantisce l’assenza di interruzioni o errori. Nei limiti consentiti dalla
+                legge, AG SERVIZI non è responsabile per danni indiretti, perdita di dati o mancati profitti derivanti
+                dall’uso o dall’impossibilità di usare il Software.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>7. Sospensione e cessazione</h3>
-        <p>
-            In caso di violazione dei presenti termini, AG SERVIZI potrà sospendere o revocare l’accesso al Software,
-            ferma restando la tutela dei propri diritti.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>7. Sospensione e cessazione</h3>
+            <p>
+                In caso di violazione dei presenti termini, AG SERVIZI potrà sospendere o revocare l’accesso al Software,
+                ferma restando la tutela dei propri diritti.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>8. Privacy e trattamento dati</h3>
-        <p>
-            Il trattamento dei dati personali avviene secondo quanto indicato nella Privacy Policy disponibile nella
-            relativa sezione.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>8. Privacy e trattamento dati</h3>
+            <p>
+                Il trattamento dei dati personali avviene secondo quanto indicato nella Privacy Policy disponibile nella
+                relativa sezione.
+            </p>
+        </section>
 
-    <section class="page__section">
-        <h3>9. Legge applicabile e foro competente</h3>
-        <p>
-            I presenti termini sono regolati dalla legge italiana. Per qualsiasi controversia relativa all’interpretazione
-            o esecuzione dei presenti termini, sarà competente il foro territorialmente previsto dalla normativa vigente,
-            salvo diversa disposizione inderogabile.
-        </p>
-    </section>
+        <section class="page__section">
+            <h3>9. Legge applicabile e foro competente</h3>
+            <p>
+                I presenti termini sono regolati dalla legge italiana. Per qualsiasi controversia relativa all’interpretazione
+                o esecuzione dei presenti termini, sarà competente il foro territorialmente previsto dalla normativa vigente,
+                salvo diversa disposizione inderogabile.
+            </p>
+        </section>
+    </div>
 </section>
